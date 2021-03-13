4d7d0cc041c9ed7f99217c6261520926f96da74b
KielanT <34215034+KielanT@users.noreply.github.com>
2021-03-13 19:53:51 +0000

    Game Class Setup again

--- CMonopolyGame.h
+++ CMonopolyGame.h
@@ -1,6 +1,26 @@
 #pragma once
+#include <fstream>
+#include <vector>
+#include <memory>
+#include "CSquare.h"
+#include "CSquareFactory.h"
+#include "CPlayer.h"
+
+
 class CMonopolyGame
 {
+private:
+	typedef vector<unique_ptr<CSquare>> CSquareVector;
+	const char mPOUND = 156;
+	const string mFILE_SQUARES = "monopoly.txt"; // Used for storing the file that stores the square data
+	CSquareVector pSquares;
 
+public:
+	CMonopolyGame();
+	void Game();
+
+private:
+	void Round(shared_ptr<CPlayer> Player, shared_ptr<CPlayer> OtherPlayer, CSquareVector& squares);
+	void GameOver(shared_ptr<CPlayer> PlayerOne, shared_ptr<CPlayer> PlayerTwo);
+	void ReadSquaresFile(string fileName, CSquareVector& squares);
 };
-
